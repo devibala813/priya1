0e0b697c202788f9bc3d7f248065e64a30c6b497
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-03 16:22:30 +0530

    Update 75.py

--- 75.py
+++ 75.py
@@ -1,11 +1,11 @@
-dd=str(input())
-s=list(dd)
-r=len(dd)
+dd1=str(input())
+s=list(dd1)
+r=len(dd1)
 p=""
 if r%2==0:
-   d[int(r/2)]=="*"
-   d[int(r//2)]=="*"
+   s[int(r/2)]=="*"
+   s[int(r//2)]=="*"
 else:
-   d[int(r/2)]=="*"
+   s[int(r/2)]=="*"
 p=p.join(s)
 print(p)
