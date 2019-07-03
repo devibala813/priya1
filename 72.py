e35b8debc11f244b2427b8c79ed4c6fdc88a5b9c
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-03 15:23:32 +0530

    Update 72.py

--- 72.py
+++ 72.py
@@ -1,8 +1,9 @@
 n1=input()
 for i in n1:
    if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i="A" or i="E" or i=="I" i=="O" or i=="U"):
-      print("yes")
-      break
-else:
-   print("no")
+         print("yes")
+         break
+ else:
+      print("no")
+  
       
